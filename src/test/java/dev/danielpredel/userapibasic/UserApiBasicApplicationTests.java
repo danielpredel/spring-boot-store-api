2378fa96f9c1d598068c6caf0da9104ae5838767
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-04-17 14:59:28 -0600

    Create Repo

---
+++ src/test/java/dev/danielpredel/userapibasic/UserApiBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielpredel.userapibasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
