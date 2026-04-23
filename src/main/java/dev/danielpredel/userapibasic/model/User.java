3706e4c72700c874b39c4f21dc293f64dfd19dd0
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-04-23 13:22:08 -0600

    Create User model

---
+++ src/main/java/dev/danielpredel/userapibasic/model/User.java
@@ -0,0 +1,34 @@
+package dev.danielpredel.userapibasic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class User {
+    private final Long id;
+    @Setter
+    private String name;
+    @Setter
+    private String email;
+    @Setter
+    private String password;
+    @Setter
+    private String address;
+
+    public User(String name, String email, String password, String address) {
+        this(null, name, email, password, address);
+    }
+
+    public User(Long id, String name, String email, String password, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", name='" + name + "', email='" + email + "', password='" + "*".repeat(16) + "', address='" + address + "'}";
+    }
+}
