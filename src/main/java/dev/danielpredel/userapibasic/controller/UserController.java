fe17d2f5b4936397528632ac5e0efc8ae2bd7867
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-04 16:28:30 -0600

    Add min value for page and min-max values for size in GET users controller

--- src/main/java/dev/danielpredel/userapibasic/controller/UserController.java
+++ src/main/java/dev/danielpredel/userapibasic/controller/UserController.java
@@ -4,17 +4,20 @@ import dev.danielpredel.userapibasic.dto.UserRequest;
 import dev.danielpredel.userapibasic.dto.UserResponse;
 import dev.danielpredel.userapibasic.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
-
+@Validated
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -35,13 +38,11 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Page<UserResponse>> findAll(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "0") @Min(0) int page,
+            @RequestParam(defaultValue = "10") @Min(1) @Max(50) int size,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "asc") String direction
     ) {
-        size = Math.min(size, 50);
-
         List<String> allowed = List.of("id", "email", "name");
         if (!allowed.contains(sortBy)) sortBy = "id";
 
