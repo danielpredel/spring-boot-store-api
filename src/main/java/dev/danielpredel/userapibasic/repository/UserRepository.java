dd5617fb4a38fa30fda4d408161e011291429051
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-03 22:14:03 -0600

    Define method findByEmail in User repository

--- src/main/java/dev/danielpredel/userapibasic/repository/UserRepository.java
+++ src/main/java/dev/danielpredel/userapibasic/repository/UserRepository.java
@@ -3,8 +3,11 @@ package dev.danielpredel.userapibasic.repository;
 import dev.danielpredel.userapibasic.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
     boolean existsByEmailAndIdNot(String email, Long id);
+    Optional<UserEntity> findByEmail(String email);
 }
