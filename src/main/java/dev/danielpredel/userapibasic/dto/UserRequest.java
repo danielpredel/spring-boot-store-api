0a7c8bd5b1cb8afb77d0c3d9f0d3d19072558d93
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-05 16:36:51 -0600

    Add string size limitations to UserRequest DTO

--- src/main/java/dev/danielpredel/userapibasic/dto/UserRequest.java
+++ src/main/java/dev/danielpredel/userapibasic/dto/UserRequest.java
@@ -5,13 +5,20 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest (
-    @NotBlank(message = "Can't be blank.") String name,
+    @NotBlank
+    @Size(max = 100)
+    String name,
 
-    @NotBlank(message = "Can't be blank.")
-    @Email(message = "Invalid format.") String email,
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    String email,
 
-    @NotBlank(message = "Can't be blank.")
-    @Size(min = 8, message = "Too short.") String password,
+    @NotBlank
+    @Size(min = 8, max = 64)
+    String password,
 
-    @NotBlank(message = "Can't be blank.") String address
+    @NotBlank
+    @Size(max = 150)
+    String address
 ) {}
