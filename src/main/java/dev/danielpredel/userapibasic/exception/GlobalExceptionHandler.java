ced2e26d01ad2fda91ddba751ab5f818359fa374
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-05 17:23:15 -0600

    Update GlobalExceptionHandler to adapt to errors in ErrorResponse DTO as Map<String, List<String>>

--- src/main/java/dev/danielpredel/userapibasic/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/danielpredel/userapibasic/exception/GlobalExceptionHandler.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.StreamSupport;
 
@@ -35,10 +37,11 @@ public class GlobalExceptionHandler {
         String message = "Invalid Requested Data";
         int status = HttpStatus.BAD_REQUEST.value();
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        Map<String, String> errors = new HashMap<>();
+        Map<String, List<String>> errors = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(fieldError ->
-                errors.put(fieldError.getField(), fieldError.getDefaultMessage())
+                errors.computeIfAbsent(fieldError.getField(), key -> new ArrayList<>())
+                        .add(fieldError.getDefaultMessage())
         );
 
         ErrorResponse response = new ErrorResponse(message, status, timestamp, errors);
@@ -66,15 +69,15 @@ public class GlobalExceptionHandler {
         String message = "Invalid Requested Data";
         int status = HttpStatus.BAD_REQUEST.value();
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        Map<String, String> errors = new HashMap<>();
+        Map<String, List<String>> errors = new HashMap<>();
 
         ex.getConstraintViolations().forEach(violation -> {
             String field = StreamSupport.stream(violation.getPropertyPath().spliterator(), false)
                     .reduce((first, second) -> second)
                     .map(Object::toString)
                     .orElse("");
-            String vMessage = violation.getMessage();
-            errors.put(field, vMessage);
+            errors.computeIfAbsent(field, k -> new ArrayList<>())
+                    .add(violation.getMessage());
         });
 
         ErrorResponse response = new ErrorResponse(message, status, timestamp, errors);
