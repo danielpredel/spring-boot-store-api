83bed303f7924a13176eeb30b59b445313cbf11f
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-05 23:31:42 -0600

    feat(product): add delete product endpoint

--- src/main/java/dev/danielpredel/userapibasic/controller/ProductController.java
+++ src/main/java/dev/danielpredel/userapibasic/controller/ProductController.java
@@ -66,4 +66,10 @@ public class ProductController {
     public ResponseEntity<ProductResponse> update(@PathVariable Long id, @Valid @RequestBody ProductRequest dto) {
         return  ResponseEntity.ok(productService.update(id, dto));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
