dfc9fd173145d0760e28086f3238a66060817b4b
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-05 20:40:32 -0600

    feat(product): add field active. set table name as products

--- src/main/java/dev/danielpredel/userapibasic/entity/Product.java
+++ src/main/java/dev/danielpredel/userapibasic/entity/Product.java
@@ -1,15 +1,13 @@
 package dev.danielpredel.userapibasic.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "products")
 @Getter
 public class Product {
     @Id
@@ -28,13 +26,17 @@ public class Product {
     @Setter
     private String imageUrl;
 
+    @Setter
+    private boolean active;
+
     public Product() {}
 
-    public Product(String name, BigDecimal price, int stock, String imageUrl) {
+    public Product(String name, BigDecimal price, int stock, String imageUrl, boolean active) {
         this.name = name;
         this.price = price;
         this.stock = stock;
         this.imageUrl = imageUrl;
+        this.active = active;
     }
 
     @Override
