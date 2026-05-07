755715cc437e9d421a1fd71f97b478669726962a
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-06 23:52:57 -0600

    feat(orderItem): create OrderItem DTOs with validation

---
+++ src/main/java/dev/danielpredel/userapibasic/dto/OrderItemRequest.java
@@ -0,0 +1,13 @@
+package dev.danielpredel.userapibasic.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record OrderItemRequest(
+        @NotNull
+        Long productId,
+
+        @NotNull
+        @Min(1)
+        Integer quantity
+) {}
