49d7d972b8f3d82180783e91a08f20e585bdf1f2
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-06 23:52:04 -0600

    feat(order): create Order entity

---
+++ src/main/java/dev/danielpredel/userapibasic/entity/Order.java
@@ -0,0 +1,43 @@
+package dev.danielpredel.userapibasic.entity;
+
+import dev.danielpredel.userapibasic.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Setter
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @Setter
+    private List<OrderItem> orderItems;
+
+    @Setter
+    private BigDecimal totalAmount;
+
+    @Setter
+    private LocalDateTime purchaseDate;
+
+    @Setter
+    private OrderStatus status;
+
+    public Order() {}
+
+    public Order(LocalDateTime purchaseDate, OrderStatus status) {
+        this.purchaseDate = purchaseDate;
+        this.status = status;
+    }
+}
