e2efd253ffe4bdaaa19fe52dcc739fdef867a2db
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-07 16:52:55 -0600

    feat(order): add GET orders enpoint in OrderController

--- src/main/java/dev/danielpredel/userapibasic/controller/OrderController.java
+++ src/main/java/dev/danielpredel/userapibasic/controller/OrderController.java
@@ -4,13 +4,17 @@ import dev.danielpredel.userapibasic.dto.OrderRequest;
 import dev.danielpredel.userapibasic.dto.OrderResponse;
 import dev.danielpredel.userapibasic.service.OrderService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -28,5 +32,27 @@ public class OrderController {
         URI location = URI.create("/api/orders" + orderResponse.id());
 
         return ResponseEntity.created(location).body(orderResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<OrderResponse>> findAll(
+            @RequestParam(defaultValue = "0") @Min(0) int page,
+            @RequestParam(defaultValue = "10") @Min(1) @Max(50) int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        List<String> allowed = List.of("id", "purchaseDate", "status", "totalAmount");
+        if (!allowed.contains(sortBy)) sortBy = "id";
+
+        Sort sort = Sort.by(
+                "desc".equalsIgnoreCase(direction)
+                        ? Sort.Direction.DESC
+                        : Sort.Direction.ASC,
+                sortBy
+        );
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return ResponseEntity.ok(orderService.findAll(pageable));
     }
 }
