47670fca50298ed61191dcb324aed3f911fcfe04
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-06 23:54:28 -0600

    feat(order): create Order mapper to map both Order and Order DTOs

---
+++ src/main/java/dev/danielpredel/userapibasic/mapper/OrderMapper.java
@@ -0,0 +1,28 @@
+package dev.danielpredel.userapibasic.mapper;
+
+import dev.danielpredel.userapibasic.dto.OrderItemResponse;
+import dev.danielpredel.userapibasic.dto.OrderResponse;
+import dev.danielpredel.userapibasic.entity.Order;
+import dev.danielpredel.userapibasic.entity.OrderItem;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class OrderMapper {
+    public OrderResponse toResponse(Order order) {
+        List<OrderItemResponse> items = order.getOrderItems()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+        return new OrderResponse(order.getId(), items, order.getTotalAmount(), order.getPurchaseDate());
+    }
+
+    public OrderItemResponse toResponse(OrderItem orderItem) {
+        return new OrderItemResponse(
+                orderItem.getProduct().getId(),
+                orderItem.getProduct().getName(),
+                orderItem.getQuantity(),
+                orderItem.getPriceAtPurchase());
+    }
+}
