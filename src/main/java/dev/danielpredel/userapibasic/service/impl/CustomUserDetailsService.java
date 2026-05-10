40bebb23f5e9d70f240786c6dbc5b843c4542815
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-10 02:05:59 -0600

    feat(auth): add CustomUserDetailsService

---
+++ src/main/java/dev/danielpredel/userapibasic/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package dev.danielpredel.userapibasic.service.impl;
+
+import dev.danielpredel.userapibasic.entity.User;
+import dev.danielpredel.userapibasic.exception.ResourceNotFoundException;
+import dev.danielpredel.userapibasic.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email) throws ResourceNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(
+                        new SimpleGrantedAuthority(
+                                "ROLE_" + user.getRole().name()
+                        )
+                )
+        );
+    }
+}
