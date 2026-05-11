0b5d795714db72552732b34514e03febe5ff4765
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-11 00:28:03 -0600

    feat(auth): add admin seeder

---
+++ src/main/java/dev/danielpredel/userapibasic/config/AdminSeeder.java
@@ -0,0 +1,44 @@
+package dev.danielpredel.userapibasic.config;
+
+import dev.danielpredel.userapibasic.entity.User;
+import dev.danielpredel.userapibasic.enums.Role;
+import dev.danielpredel.userapibasic.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AdminSeeder {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    @Bean
+    CommandLineRunner createAdmin() {
+        return args -> {
+
+            String name = "Administrator";
+            String email = adminEmail;
+            String password = passwordEncoder.encode(adminPassword);
+            String address = "No Need For An Address";
+            Role role = Role.ADMIN;
+
+            if (userRepository.findByEmail(email).isEmpty()) {
+                User admin = new User(name, email, password, address, role);
+
+                userRepository.save(admin);
+
+                System.out.println("Admin user created");
+            }
+        };
+    }
+}
