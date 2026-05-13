3fe17bb9e43d60e0bef4d2807b91b5b60e1cfe31
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-12 20:02:52 -0600

    feat(order): add method to get orders for a certain userId

--- src/main/java/dev/danielpredel/userapibasic/repository/OrderRepository.java
+++ src/main/java/dev/danielpredel/userapibasic/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package dev.danielpredel.userapibasic.repository;
 
 import dev.danielpredel.userapibasic.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    public Page<Order> findByUserId(Long userId, Pageable pageable);
 }
