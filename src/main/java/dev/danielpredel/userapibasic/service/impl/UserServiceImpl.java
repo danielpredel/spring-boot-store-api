51162ebce3bad73a48d70395ea02b0e174e7acde
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-13 18:28:36 -0600

    ref(user): remove unused import

--- src/main/java/dev/danielpredel/userapibasic/service/impl/UserServiceImpl.java
+++ src/main/java/dev/danielpredel/userapibasic/service/impl/UserServiceImpl.java
@@ -13,7 +13,6 @@ import dev.danielpredel.userapibasic.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
