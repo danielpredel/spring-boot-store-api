324c01b30c019d510c51066829978eea97047881
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-13 18:59:09 -0600

    feat(user): add method to fin by email and active in repository

--- src/main/java/dev/danielpredel/userapibasic/repository/UserRepository.java
+++ src/main/java/dev/danielpredel/userapibasic/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByEmailAndIdNot(String email, Long id);
     Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndActive(String email, boolean active);
 }
