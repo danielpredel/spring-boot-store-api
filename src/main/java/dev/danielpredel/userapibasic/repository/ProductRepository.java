5855c572ada7aefe7fd33f44075368a8be146fba
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-14 16:48:24 -0600

    feat(product): add method to find products with active=true

--- src/main/java/dev/danielpredel/userapibasic/repository/ProductRepository.java
+++ src/main/java/dev/danielpredel/userapibasic/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package dev.danielpredel.userapibasic.repository;
 
 import dev.danielpredel.userapibasic.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByActiveTrue(Pageable pageable);
 }
