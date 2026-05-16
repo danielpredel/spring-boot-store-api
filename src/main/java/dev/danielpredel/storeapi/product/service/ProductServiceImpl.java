b5e93eae772aa6c1c211bf802f3b212ea481ec62
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-16 16:43:47 -0600

    ref(product)!: move public service impl

--- src/main/java/dev/danielpredel/storeapi/product/service/ProductServiceImpl.java
+++ src/main/java/dev/danielpredel/storeapi/product/service/ProductServiceImpl.java
@@ -1,4 +1,4 @@
-package dev.danielpredel.storeapi.service.impl;
+package dev.danielpredel.storeapi.product.service;
 
 import dev.danielpredel.storeapi.product.dto.ProductResponse;
 import dev.danielpredel.storeapi.product.entity.Product;
