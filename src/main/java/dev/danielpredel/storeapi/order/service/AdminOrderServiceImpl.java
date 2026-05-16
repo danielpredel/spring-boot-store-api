5e8129cd34a5b4a5cf0a6d1e1008ae5282f362d4
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-16 17:14:43 -0600

    ref(order)!: move ADMIN service

--- src/main/java/dev/danielpredel/storeapi/order/service/AdminOrderServiceImpl.java
+++ src/main/java/dev/danielpredel/storeapi/order/service/AdminOrderServiceImpl.java
@@ -1,4 +1,4 @@
-package dev.danielpredel.storeapi.service.impl;
+package dev.danielpredel.storeapi.order.service;
 
 import dev.danielpredel.storeapi.order.dto.admin.AdminOrderResponse;
 import dev.danielpredel.storeapi.order.entity.Order;
