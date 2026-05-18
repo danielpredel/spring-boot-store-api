b207881384cba21f0ba761b6efc9e38974e8c1bd
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-17 23:57:06 -0600

    feat(register): set minimum secure password requirements

--- src/main/java/dev/danielpredel/storeapi/user/dto/auth/RegisterRequest.java
+++ src/main/java/dev/danielpredel/storeapi/user/dto/auth/RegisterRequest.java
@@ -2,6 +2,7 @@ package dev.danielpredel.storeapi.user.dto.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
@@ -16,6 +17,10 @@ public record RegisterRequest(
 
     @NotBlank
     @Size(min = 8, max = 64)
+    @Pattern(
+            regexp = "^(?=(?:.*[A-Z]){2,})(?=(?:.*[a-z]){2,})(?=(?:.*\\d){2,})(?=(?:.*[@$!%*?&]){2,}).*$",
+            message = "Must have at least 2 uppercase, 2 lowercase, 2 digits, and 2 symbols"
+    )
     String password,
 
     @NotBlank
