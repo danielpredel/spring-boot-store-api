380a4ed19bfd8863fba5821e3baa0140b7a32f79
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-18 17:24:35 -0600

    feat(product): add ApiResponse DTO in user controller (ADMIN role)

--- src/main/java/dev/danielpredel/storeapi/user/controller/AdminUserController.java
+++ src/main/java/dev/danielpredel/storeapi/user/controller/AdminUserController.java
@@ -1,5 +1,6 @@
 package dev.danielpredel.storeapi.user.controller;
 
+import dev.danielpredel.storeapi.common.dto.ApiResponse;
 import dev.danielpredel.storeapi.user.dto.admin.AdminUserResponse;
 import dev.danielpredel.storeapi.user.service.AdminUserService;
 import jakarta.validation.constraints.Max;
@@ -8,10 +9,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @Validated
@@ -25,7 +28,7 @@ public class AdminUserController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<AdminUserResponse>> findAll(
+    public ResponseEntity<ApiResponse<Page<AdminUserResponse>>> findAll(
             @RequestParam(defaultValue = "0") @Min(0) int page,
             @RequestParam(defaultValue = "10") @Min(1) @Max(50) int size,
             @RequestParam(defaultValue = "id") String sortBy,
@@ -42,11 +45,25 @@ public class AdminUserController {
         );
 
         Pageable pageable = PageRequest.of(page, size, sort);
-        return ResponseEntity.ok(adminUserService.findAll(pageable));
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Users retrieved successfully",
+                        HttpStatus.OK.value(),
+                        Instant.now().toString(),
+                        adminUserService.findAll(pageable)
+                )
+        );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AdminUserResponse> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(adminUserService.findById(id));
+    public ResponseEntity<ApiResponse<AdminUserResponse>> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                new  ApiResponse<>(
+                        "User retrieved successfully",
+                        HttpStatus.OK.value(),
+                        Instant.now().toString(),
+                        adminUserService.findById(id)
+                )
+        );
     }
 }
