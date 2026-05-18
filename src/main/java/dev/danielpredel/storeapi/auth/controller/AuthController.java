72627a7daec22a395b2eb8b9225309494c3fe997
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-18 17:39:36 -0600

    feat(auth): add ApiResponse DTO in auth controller

--- src/main/java/dev/danielpredel/storeapi/auth/controller/AuthController.java
+++ src/main/java/dev/danielpredel/storeapi/auth/controller/AuthController.java
@@ -2,12 +2,13 @@ package dev.danielpredel.storeapi.auth.controller;
 
 import dev.danielpredel.storeapi.auth.dto.AuthRequest;
 import dev.danielpredel.storeapi.auth.dto.AuthResponse;
+import dev.danielpredel.storeapi.common.dto.ApiResponse;
 import dev.danielpredel.storeapi.user.dto.auth.RegisterRequest;
-import dev.danielpredel.storeapi.user.dto.UserResponse;
 import dev.danielpredel.storeapi.auth.service.AuthService;
 import dev.danielpredel.storeapi.user.dto.auth.RegisterResponse;
 import dev.danielpredel.storeapi.user.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,7 +31,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> create(@Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity<ApiResponse<RegisterResponse>> create(@Valid @RequestBody RegisterRequest request) {
         RegisterResponse user = userService.save(request);
 
         URI location = ServletUriComponentsBuilder
@@ -38,11 +40,25 @@ public class AuthController {
                 .buildAndExpand(user.id())
                 .toUri();
 
-        return ResponseEntity.created(location).body(user);
+        return ResponseEntity.created(location).body(
+                new ApiResponse<>(
+                        "User registered successfully",
+                        HttpStatus.OK.value(),
+                        Instant.now().toString(),
+                        user
+                )
+        );
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request) {
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> login(@Valid @RequestBody AuthRequest request) {
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Login successful",
+                        HttpStatus.OK.value(),
+                        Instant.now().toString(),
+                        authService.login(request)
+                )
+        );
     }
 }
