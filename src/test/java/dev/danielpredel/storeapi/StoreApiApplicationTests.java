caa86cd6b5b7b002c9c4cb1a772732392184d557
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-19 18:02:41 -0600

    ref(test): rename test class

--- src/test/java/dev/danielpredel/storeapi/StoreApiApplicationTests.java
+++ src/test/java/dev/danielpredel/storeapi/StoreApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApiBasicApplicationTests {
+class StoreApiApplicationTests {
 
     @Test
     void contextLoads() {
