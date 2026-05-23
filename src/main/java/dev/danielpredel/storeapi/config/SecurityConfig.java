3c06b6b98d226e81a190fc07c8d4803b962efac7
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-23 00:08:45 -0600

    feat(security): add public endpoint for swagger/openapi resources

--- src/main/java/dev/danielpredel/storeapi/config/SecurityConfig.java
+++ src/main/java/dev/danielpredel/storeapi/config/SecurityConfig.java
@@ -56,6 +56,13 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
+                        // Swagger/OpenAPI
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
                         // Auth
                         .requestMatchers("/auth/**").permitAll()
 
