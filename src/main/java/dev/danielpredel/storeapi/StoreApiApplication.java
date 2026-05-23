af7599a1e9291a7d67080e7f79970ce668e1da43
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-05-23 00:19:58 -0600

    feat(docs): add annotations for documentation

--- src/main/java/dev/danielpredel/storeapi/StoreApiApplication.java
+++ src/main/java/dev/danielpredel/storeapi/StoreApiApplication.java
@@ -1,13 +1,21 @@
 package dev.danielpredel.storeapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Store API",
+                version = "1.0",
+                description = "API documentation for managing products, users and orders"
+        )
+)
 @SpringBootApplication
 public class StoreApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApiApplication.class, args);
     }
-
 }
